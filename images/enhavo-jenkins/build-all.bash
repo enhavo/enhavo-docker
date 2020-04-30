632a7d4eebbc604eb66eb48d6b0b04b5cb879d13
Gerhard Seidel <gseidel.message@googlemail.com>
2020-04-30 18:17:19 +0800

    update fist

--- images/enhavo-jenkins/build-all.bash
+++ images/enhavo-jenkins/build-all.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker build --no-cache -t enhavo/enhavo-jenkins:base base
 docker build --no-cache -t enhavo/enhavo-jenkins:7.2-latest 7.2
 docker build --no-cache -t enhavo/enhavo-jenkins:7.3-latest 7.3
